1bc4a78c20b8134bb1c092badc5f6debe6a48099
刘鹤翔 <liuhexiang@bytedance.com>
2023-11-26 14:05:55 +0800

    feat: remove fixed address, read from record

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import { useState, useContext, useEffect } from 'react';
-import { Contract, ethers } from 'ethers';
+import { ethers } from 'ethers';
 import { useQuery } from '@apollo/client';
 import { Spin } from 'antd';
 import { GET_ACTIVE_ITEM } from '@/api';
 import NftCard from '@/components/nft-card';
-import contractAddress from '@/constants/contractAddress.json';
 import contractAbi from '@/constants/abi.json';
 import { Web3WalletContext } from './provider';
 import { NftInterface, TokenMetaData, NftCardProps } from '@/types';
@@ -19,21 +18,21 @@ export default function Home() {
   useRefetchData(refetch);
 
   const initNFTList = async () => {
-    const BasicNft = new ethers.Contract(
-      //@ts-ignore
-      contractAddress[chainId]?.basicNFTAddress,
-      contractAbi.basicNFTAbi,
-      signer
-    );
     const req: any[] = [];
     data?.activeItems.forEach((nftItem: NftInterface) => {
-      req.push(getNftPros(BasicNft, nftItem));
+      req.push(getNftPros(nftItem));
     });
     const nftList = await Promise.all(req);
     setNftList(nftList);
   };
 
-  const getNftPros = async (contract: Contract, nftItem: NftInterface) => {
+  const getNftPros = async (nftItem: NftInterface) => {
+    const contract = new ethers.Contract(
+      //@ts-ignore
+      nftItem.nftAddress,
+      contractAbi.basicNFTAbi,
+      signer
+    );
     const tokenURI: string = await contract.tokenURI(nftItem.tokenId);
     if (tokenURI) {
       const tokenURIRes: TokenMetaData = await (
