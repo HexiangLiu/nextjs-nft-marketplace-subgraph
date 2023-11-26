7821f98b989367e5849b2cc3c380b8936f7b69e7
刘鹤翔 <liuhexiang@bytedance.com>
2023-11-26 18:37:23 +0800

    feat: a little bit format

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <nav className='p-5 mb-4 flex flex-row justify-between items-center bg-black'>
+    <nav className='px-5 mb-4 flex flex-row justify-between items-center bg-black'>
       <h1 className='font-bold text-3xl text-white'>NFT Marketplace</h1>
-      <Link className='text-white font-bold' href='/'>
+      <Link className='text-white font-bold no-underline' href='/'>
         Home
       </Link>
-      <Link className='text-white font-bold' href='/sell'>
+      <Link className='text-white font-bold no-underline' href='/sell'>
         Sell NFT
       </Link>
       <w3m-button />
